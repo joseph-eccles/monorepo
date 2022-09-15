5de82167057cac8eacdb12a24ae41d2a7297a4b7
Eccles, Joseph <joseph.eccles@mastercard.com>
2022-09-15 14:57:24 +0100

    feat: add css

---
+++ packages/css/gulpfile.js
@@ -0,0 +1,57 @@
+const gulp = require("gulp");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssvariables = require("postcss-css-variables");
+const cssImport = require("postcss-import");
+const rename = require("gulp-rename");
+const cleanCSS = require("gulp-clean-css");
+
+/* compile css into one file with hard coded values, 
+runs autoprefixer to add browser specific prefixes */
+gulp.task("css", async function () {
+  return gulp
+    .src("src/styles.css")
+    .pipe(postcss([cssImport, cssvariables, autoprefixer]))
+    .pipe(rename("made.css"))
+    .pipe(gulp.dest("dist/2.0.0/"));
+});
+
+
+/* compile css into one file with CSS variables values */
+gulp.task("css-vars", async function () {
+  return gulp
+    .src("src/styles.css")
+    .pipe(postcss([cssImport, autoprefixer]))
+    .pipe(rename("made-css-variables.css"))
+    .pipe(gulp.dest("dist/2.0.0/"));
+});
+
+/* compile css into one minified file with hard coded values */
+gulp.task("css-min", async function () {
+  return gulp
+    .src("src/styles.css")
+    .pipe(postcss([cssImport, cssvariables, autoprefixer]))
+    .pipe(cleanCSS({ compatibility: "ie11" }))
+    .pipe(rename("made.min.css"))
+    .pipe(gulp.dest("dist/2.0.0/"));
+});
+
+/* compile css into one minified file with CSS variables values */
+gulp.task("css-vars-min", async function () {
+  return gulp
+    .src("src/styles.css")
+    .pipe(postcss([cssImport, autoprefixer]))
+    .pipe(cleanCSS({ compatibility: "ie11" }))
+    .pipe(rename("made-css-variables.min.css"))
+    .pipe(gulp.dest("dist/2.0.0/"));
+});
+
+/* copy css from src file to dist folder */
+gulp.task("copy-src", function () {
+  return gulp
+    .src(["src/**"], { base: "./" })
+    .pipe(gulp.dest("./dist/2.0.0/"));
+});
+
+/* run all of the tasks */
+gulp.task("default", gulp.parallel("css", "css-min", "css-vars", "css-vars-min", "copy-src"));
